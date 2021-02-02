db8b2724dec6c869c539fa770f034af9ee2e8dd1
sathishc58 <sathishc58@gmail.com>
2021-02-02 09:14:34 +0530

    Update HelloWorld1.java

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -2,6 +2,6 @@ import java.io.*;
 
 public class HelloWorld1 {
     public static void main(String []a) {
-        System.out.printl("Hello world from java branch");
+        System.out.println("Hello world from java branch");
     }
 }
